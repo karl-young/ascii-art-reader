feeaf6f6c7c28e64139a1205b6c01e9e986d8188
Karl Young <142771183+karl-young@users.noreply.github.com>
2023-10-13 16:41:15 +1300

    Added code to make read the data files woo

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import prompt from 'prompt'
-import { readdir } from 'node:fs/promises'
+import { readdir, readFile } from 'node:fs/promises'
+
+// welcome message function
 
 export function getWelcomeMessage() {
   return 'Welcome'
 }
+
+// get the file names and return them
 
 export async function getFileNames() {
   const fileNames = await readdir('./data')
@@ -14,11 +18,40 @@ export async function getFileNames() {
   return fileNamesWithoutTxt
 }
 
-;(() => {
+// Get input function
+async function getInput() {
+  prompt.message = ''
+  prompt.delimiter = ': '
+  prompt.start()
+
+  const choice = {
+    name: 'choice',
+    hidden: false,
+    message: 'Please type the artwork you want to see',
+  }
+  return await prompt.get(choice)
+}
+// general things working and catching errors
+async function main() {
   try {
     console.log(getWelcomeMessage())
     getFileNames()
+    const result = await getInput()
+    // console.log('You chose', result.choice)
+    console.log(await readsChoice(result.choice))
+  } catch (error) {
+    console.log('No such file, please choose from the options above')
+  }
+}
+main()
+
+// Need to make the files accessable
+// need funcition that sees the choice slecetes the file with that name,
+// console.logs the image from the file
+async function readsChoice(userChoice) {
+  try {
+    return readFile(`./data/${userChoice}.txt`, 'utf-8')
   } catch (error) {
     console.log(error)
   }
-})()
+}
